ec10b62c44b6045b3ebe0e89e72bf20627f04560
Wachiraya <Wachiraya.nc@mail.kmutt.ac.th>
2019-11-08 00:18:26 +0700

    Form 70%

--- javascript/Place.js
+++ javascript/Place.js
@@ -1,15 +1,36 @@
 var Place;
+var Rule = document.getElementById('checkrule')
+var Elec = document.getElementById('electric')
 function Select(){
     var Val = $('#Sel').val();
+    var S1 = document.getElementById('img-1')
+    var S2 = document.getElementById('img-2')
+    var S3 = document.getElementById('img-3')
+    var S4 = document.getElementById('img-4')
+    var S5 = document.getElementById('img-5')
     if(Val == "0"){
         Place = "0";  
-        document.getElementById('img').src='../img/KMUTT.png';
+        S1.src='../img/Namo.jpg';
+        S2.src='../img/Namo.jpg';
+        S3.src='../img/Namo.jpg';
+        S4.src='../img/Namo.jpg';
+        S5.src='../img/Namo.jpg';
     }
     else if(Val == "1"){
         Place = "1";
+        S1.src='../img/KMUTT.png';
+        S2.src='../img/Aon.jpg';
+        S3.src='../img/Wa.jpg';
+        S4.src='../img/God.jpg';
+        S5.src='../img/chit.jpg';
     }
     else if(Val == "2"){
         Place = "2";
+        S2.src='../img/KMUTT.png';
+        S3.src='../img/Aon.jpg';
+        S1.src='../img/Wa.jpg';
+        S4.src='../img/God.jpg';
+        S5.src='../img/chit.jpg';
     }
     else if(Val == "3"){
         Place = "3";
@@ -28,5 +49,11 @@ function Submit(){
     else if(Place == "3"){
         alert("3");
     }
-    else{alert("ERROR");}
+    //else{alert("ERROR");}
+    if(Rule != "1"){
+        $('#alert').show();
+    }
+
+    if(Elec!='1'){
+    }
 }
