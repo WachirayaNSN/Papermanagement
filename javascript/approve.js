394db3029274d91b41d5ccf06d8a689027a9ff02
Wachiraya <Wachiraya.nc@mail.kmutt.ac.th>
2019-11-09 15:31:36 +0700

    Approve

---
+++ javascript/approve.js
@@ -0,0 +1,5 @@
+//rgb(92, 238, 97)
+document.getElementById('A1').style.backgroundColor = "rgb(92, 238, 97)";
+document.getElementById('A2').style.backgroundColor = "rgb(92, 238, 97)";
+document.getElementById('A3').style.fontSize = '40px'
+document.getElementById('A3').style.backgroundColor = "rgb(250, 50, 25)";
