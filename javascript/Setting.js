36254697dda4fc2958a9c62b599fad728957df0c
Wachiraya <Wachiraya.nc@mail.kmutt.ac.th>
2019-12-03 21:12:52 +0700

    From Place

--- javascript/Setting.js
+++ javascript/Setting.js
@@ -1,3 +1,5 @@
+
+var Data_Place = 2;
 var C  = 1;
 var Info = `<div class="collapse" id="Info1" style="margin-top:50px;">
                 <div class="card card-body">
@@ -8,22 +10,69 @@ var Info = `<div class="collapse" id="Info1" style="margin-top:50px;">
 
 document.getElementById('Main').innerHTML += Info;
 
+for(var i=0;i<=Data_Place;i++){   //      Data_Place คือ จำนวนสถานที่จาก DataBase
+    document.getElementById('Place_Card').innerHTML += `<div class="card" style="width: 50%; margin-top:10%;" >
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+<div class="carousel-inner">
+<div class="carousel-item active">
+  <img class="d-block w-100" src="../img/Namo.jpg" alt="First slide">
+</div>
+<div class="carousel-item">
+  <img class="d-block w-100" src="../img/Namo.jpg" alt="Second slide">
+</div>
+<div class="carousel-item">
+  <img class="d-block w-100" src="../img/Namo.jpg" alt="Third slide">
+</div>
+</div>
+<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+<span class="sr-only">Previous</span>
+</a>
+<a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+<span class="carousel-control-next-icon" aria-hidden="true"></span>
+<span class="sr-only">Next</span>
+</a>
+</div>
+<div class="card-body">
+  <h5 class="card-title">`+document.getElementById('Name_Place'+String(i)).value+`</h5>
+  <p class="card-text" style="font-size:105%;">อาจารย์ผู้มีสิทธิ์อนุมัติ</p>
+  <p class="card-text">`+document.getElementById('Teacher1'+String(i)).value+`</p>
+  <p class="card-text">`+document.getElementById('Teacher2'+String(i)).value+`</p>
+  <p class="card-text">`+document.getElementById('Teacher3'+String(i)).value+`</p>
+  <button class="btn btn-primary">เเก้ไข / Edit</button>
+  <button class="btn btn-danger">ลบ / Delete</button>
+</div>
+</div>
+`;
+}
     function ADD_Place(){
-       
-    document.getElementById('Place_Card').innerHTML += `<div class="card" style="width: 18rem; margin-top:10%;" >
-    <div class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="d-block w-100" src="`+'../img/Namo.jpg'+`" alt="First slide" style="width:10%;height:10%;">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="`+'../img/Namo.jpg'+`" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100" src="`+'../img/Namo.jpg'+`" alt="Third slide">
-            </div>
-        </div>
+    if(document.getElementById('Name_Place').value == ''){
+        alert('การเพิ่มผิดพลาด กรุณากรอกชื่อสถานที่')
+        event.preventDefault();
+    }
+    else{
+        document.getElementById('Place_Card').innerHTML += `<div class="card" style="width: 50%; margin-top:10%;" >
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="../img/Namo.jpg" alt="First slide">
     </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../img/Namo.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="../img/Namo.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
     <div class="card-body">
       <h5 class="card-title">`+document.getElementById('Name_Place').value+`</h5>
       <p class="card-text" style="font-size:105%;">อาจารย์ผู้มีสิทธิ์อนุมัติ</p>
@@ -35,6 +84,18 @@ document.getElementById('Main').innerHTML += Info;
     </div>
   </div>
 `;
+    }
+}
+
+function ADD_Back(){
+    if(document.getElementById('Name_Place').value == ''){
+        alert('การเพิ่มผิดพลาด กรุณากรอกชื่อสถานที่')
+        event.preventDefault();
+    }
+    else{
+        // Up data ขึ้น Data Base
+    }
+    
 }
 function Teacher_1(){
     if(document.getElementById('Teacher1').value == ''){
